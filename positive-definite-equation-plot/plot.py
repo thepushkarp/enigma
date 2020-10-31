7ec55a727444a7c37eb9778f00fad7548e2ee84e
Pushkar Patel <thepushkarp@gmail.com>
2020-10-31 18:16:40 +0530

    Add f-strings

--- positive-definite-equation-plot/plot.py
+++ positive-definite-equation-plot/plot.py
@@ -12,7 +12,7 @@ def plot():
 
     # Makes a plot for each value of w
     for i in range(20 + 1):
-        print('Plotting Plot {}'.format(i))
+        print(f'Plotting Plot {i}')
         folder_name = os.path.join('csvs', 'csv{:0>2d}'.format(i))
         fig = plt.figure()
         ax1 = fig.add_subplot(1, 1, 1, projection='3d')
